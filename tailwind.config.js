bec9e21414cc115bdd2b92e426c61e78b164bc07
Mohammad Mohammadzade <mohammad1382217@gmail.com>
2024-03-12 20:53:47 +0330

    add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
