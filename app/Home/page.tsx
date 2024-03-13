cb1d8e245b9b5f5e99f728e7e8e606b417d2c46c
amirali <amirali.programer2017@gmail.com>
2024-03-13 15:54:42 +0330

    add card component and edit its style

--- app/Home/page.tsx
+++ app/Home/page.tsx
@@ -5,12 +5,12 @@ import ImageHomeHeaderLeft2 from "../assets/images/coffee.png";
 import presentage from "../assets/images/icons/percentage-square.svg";
 import foodpost from "../assets/images/foodpost.png";
 import LineSpace from "../components/lineSpace";
-import { Card } from "flowbite-react";
 import "./style.css";
+import CardComponent from "../components/Card";
 
 const Home: React.FC = () => {
   return (
-    <div className="flex flex-col mt-40 justify-center ">
+    <div className="flex flex-col mt-10 justify-center ">
       <div className="  flex justify-center">
         <img src={ImageHomeHeaderRight} className="w-2/4 rounded-t-lg" alt="" />
         <div className="flex flex-col w-1/4">
@@ -33,37 +33,7 @@ const Home: React.FC = () => {
             text={"پیشنهادهای ویژه"}
             icon={<img className="w-7 h-7" src={presentage}></img>}
           />
-          <Card
-            className="max-w-72 mt-10 mb-10 p-0 card"
-            //   imgAlt="Apple Watch Series 7 in colors pink, silver, and black"
-            imgSrc=""
-          >
-            <img src={foodpost} alt="" />
-            <a href="#">
-              <h5 className="text-xl mr-3 font-semibold tracking-tight text-gray-900 dark:text-white">
-                باغ رستوران بهشت
-              </h5>
-            </a>
-            <div className="mb-5 mt-2.5  flex items-center justify-center">
-              <p>امتیاز :</p>
-
-              <span className="ml-1 mr-1  text-xs font-semibold text-cyan-800 dark:bg-cyan-200 dark:text-cyan-800">
-                4.1
-              </span>
-              <svg
-                className="h-5 w-5 text-yellow-300"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-              </svg>
-
-              <span className="mr-5">
-                از 10% تا <span className="text-lg">تخفیف <span className="text-[#7F38B7]"> %20</span></span>
-              </span>
-            </div>
-          </Card>
+          <CardComponent text={'باغ رستوران بهشت'} rate={'4.0'} offer={["10%", "20%"]} img={foodpost} />
         </div>
       </div>
     </div>
--- app/components/lineSpace.tsx
+++ app/components/lineSpace.tsx
@@ -9,12 +9,17 @@ interface LineSpaceProps {
 
 const LineSpace: React.FC<LineSpaceProps> = ({ text, icon, color }) => {
   return (
-    <div className="flex items-center justify-center px-3" style={{ color: color }}>
+    <div
+      className="flex items-center justify-center px-3"
+      style={{ color: color }}
+    >
       {icon}
       <span className={`mx-2`}>{text}</span>
-          <div style={{borderColor:color}} className="flex-1 border-b-4 border-dotted border-3 mx-3"></div>
-          
-      مشاهده بیشتر &gt;
+      <div
+        style={{ borderColor: color }}
+        className="flex-1 border-b-4 border-dotted border-3 mx-3"
+      ></div>
+      <button> مشاهده بیشتر &gt;</button>
     </div>
   );
 };
