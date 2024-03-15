55034831d241ba44db88d2af56e260fefa84cf84
----- Amirali Eshaghi ----- <114607506+aliespro@users.noreply.github.com>
2024-03-15 10:06:36 -0700

    add box login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div></div>
+    <div className='bg-[#F5F5F5] w-[100vw] h-[100vh] flex justify-center items-center'>
+
+
+      <div className='bg-[#FFFFFF] h-80 w-72 box_login rounded-md'>
+
+      </div>
+    </div>
   )
 }
 
