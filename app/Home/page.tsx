ab9b4e805bf52cb2409aeb3cb02d225b8bbb84a4
----- Amirali Eshaghi ----- <114607506+aliespro@users.noreply.github.com>
2024-03-15 08:02:42 -0700

    add mobile site size mobile mode ape Home

--- app/Home/page.tsx
+++ app/Home/page.tsx
@@ -9,15 +9,23 @@ import foodpost from "../assets/images/foodpost.png";
 import health from "../assets/images/icons/health.svg";
 import recreational from "../assets/images/icons/recreational.svg";
 import teacher from "../assets/images/icons/teacher.svg";
+import app_phone from "../assets/images/appmobileHome.png";
 import LineSpace from "../components/lineSpace";
 import CardComponent from "../components/Card";
+import { HiDownload } from "react-icons/hi";
+import { Button } from "@material-tailwind/react";
+import bazar from "../assets/bazar.svg";
 
 const Home: React.FC = () => {
   return (
-    <div className="flex flex-col mt-10 justify-center items-center">
+    <div className="flex flex-col bg-[#F5F5F5] justify-center items-center">
       <div className="flex flex-col lg:w-9/12 w-11/12 md:flex-row  px-5">
         {/* <div> */}
-        <img src={ImageHomeHeaderRight} className="md:w-8/12 rounded-t-lg" alt="" />
+        <img
+          src={ImageHomeHeaderRight}
+          className="md:w-8/12 rounded-t-lg"
+          alt=""
+        />
         <div className="flex flex-row md:flex-col md:w-4/12">
           <img
             src={ImageHomeHeaderLeft1}
@@ -44,7 +52,7 @@ const Home: React.FC = () => {
         </div>
         {/* </div> */}
       </div>
-      
+
       <div className="bg-[#8448B2] w-full my-3 flex flex-col justify-center items-center">
         <div className="mt-10 lg:w-9/12 w-11/12 ">
           <LineSpace
@@ -53,10 +61,30 @@ const Home: React.FC = () => {
             icon={<img className="w-7 h-7" src={presentage}></img>}
           />
           <div className="flex flex-row mb-1 justify-between overflow-x-scroll ">
-          <CardComponent text={'باغ رستوران بهشت'} rate={'4.0'} offer={["10%", "20%"]} img={foodpost} />
-          <CardComponent text={'باغ رستوران بهشت'} rate={'4.0'} offer={["10%", "20%"]} img={foodpost} />
-          <CardComponent text={'باغ رستوران بهشت'} rate={'4.0'} offer={["10%", "20%"]} img={foodpost} />
-          <CardComponent text={'باغ رستوران بهشت'} rate={'4.0'} offer={["10%", "20%"]} img={foodpost} />
+            <CardComponent
+              text={"باغ رستوران بهشت"}
+              rate={"4.0"}
+              offer={["10%", "20%"]}
+              img={foodpost}
+            />
+            <CardComponent
+              text={"باغ رستوران بهشت"}
+              rate={"4.0"}
+              offer={["10%", "20%"]}
+              img={foodpost}
+            />
+            <CardComponent
+              text={"باغ رستوران بهشت"}
+              rate={"4.0"}
+              offer={["10%", "20%"]}
+              img={foodpost}
+            />
+            <CardComponent
+              text={"باغ رستوران بهشت"}
+              rate={"4.0"}
+              offer={["10%", "20%"]}
+              img={foodpost}
+            />
           </div>
         </div>
       </div>
@@ -68,10 +96,30 @@ const Home: React.FC = () => {
             icon={<img className="w-7 h-7" src={foodIcon}></img>}
           />
           <div className="flex flex-row justify-between overflow-x-scroll">
-          <CardComponent text={'باغ رستوران بهشت'} rate={'4.0'} offer={["10%", "20%"]} img={foodpost} />
-          <CardComponent text={'باغ رستوران بهشت'} rate={'4.0'} offer={["10%", "20%"]} img={foodpost} />
-          <CardComponent text={'باغ رستوران بهشت'} rate={'4.0'} offer={["10%", "20%"]} img={foodpost} />
-          <CardComponent text={'باغ رستوران بهشت'} rate={'4.0'} offer={["10%", "20%"]} img={foodpost} />
+            <CardComponent
+              text={"باغ رستوران بهشت"}
+              rate={"4.0"}
+              offer={["10%", "20%"]}
+              img={foodpost}
+            />
+            <CardComponent
+              text={"باغ رستوران بهشت"}
+              rate={"4.0"}
+              offer={["10%", "20%"]}
+              img={foodpost}
+            />
+            <CardComponent
+              text={"باغ رستوران بهشت"}
+              rate={"4.0"}
+              offer={["10%", "20%"]}
+              img={foodpost}
+            />
+            <CardComponent
+              text={"باغ رستوران بهشت"}
+              rate={"4.0"}
+              offer={["10%", "20%"]}
+              img={foodpost}
+            />
           </div>
         </div>
         <div className="mt-10 mx-auto">
@@ -81,10 +129,30 @@ const Home: React.FC = () => {
             icon={<img className="w-7 h-7" src={recreational}></img>}
           />
           <div className="flex flex-row justify-between overflow-x-scroll">
-          <CardComponent text={'باغ رستوران بهشت'} rate={'4.0'} offer={["10%", "20%"]} img={foodpost} />
-          <CardComponent text={'باغ رستوران بهشت'} rate={'4.0'} offer={["10%", "20%"]} img={foodpost} />
-          <CardComponent text={'باغ رستوران بهشت'} rate={'4.0'} offer={["10%", "20%"]} img={foodpost} />
-          <CardComponent text={'باغ رستوران بهشت'} rate={'4.0'} offer={["10%", "20%"]} img={foodpost} />
+            <CardComponent
+              text={"باغ رستوران بهشت"}
+              rate={"4.0"}
+              offer={["10%", "20%"]}
+              img={foodpost}
+            />
+            <CardComponent
+              text={"باغ رستوران بهشت"}
+              rate={"4.0"}
+              offer={["10%", "20%"]}
+              img={foodpost}
+            />
+            <CardComponent
+              text={"باغ رستوران بهشت"}
+              rate={"4.0"}
+              offer={["10%", "20%"]}
+              img={foodpost}
+            />
+            <CardComponent
+              text={"باغ رستوران بهشت"}
+              rate={"4.0"}
+              offer={["10%", "20%"]}
+              img={foodpost}
+            />
           </div>
         </div>
         <div className="mt-10 mx-auto">
@@ -94,10 +162,30 @@ const Home: React.FC = () => {
             icon={<img className="w-7 h-7" src={health}></img>}
           />
           <div className="flex flex-row justify-between overflow-x-scroll">
-          <CardComponent text={'باغ رستوران بهشت'} rate={'4.0'} offer={["10%", "20%"]} img={foodpost} />
-          <CardComponent text={'باغ رستوران بهشت'} rate={'4.0'} offer={["10%", "20%"]} img={foodpost} />
-          <CardComponent text={'باغ رستوران بهشت'} rate={'4.0'} offer={["10%", "20%"]} img={foodpost} />
-          <CardComponent text={'باغ رستوران بهشت'} rate={'4.0'} offer={["10%", "20%"]} img={foodpost} />
+            <CardComponent
+              text={"باغ رستوران بهشت"}
+              rate={"4.0"}
+              offer={["10%", "20%"]}
+              img={foodpost}
+            />
+            <CardComponent
+              text={"باغ رستوران بهشت"}
+              rate={"4.0"}
+              offer={["10%", "20%"]}
+              img={foodpost}
+            />
+            <CardComponent
+              text={"باغ رستوران بهشت"}
+              rate={"4.0"}
+              offer={["10%", "20%"]}
+              img={foodpost}
+            />
+            <CardComponent
+              text={"باغ رستوران بهشت"}
+              rate={"4.0"}
+              offer={["10%", "20%"]}
+              img={foodpost}
+            />
           </div>
         </div>
         <div className="mt-10 mx-auto">
@@ -107,10 +195,30 @@ const Home: React.FC = () => {
             icon={<img className="w-7 h-7" src={teacher}></img>}
           />
           <div className="flex flex-row justify-between overflow-x-scroll">
-          <CardComponent text={'باغ رستوران بهشت'} rate={'4.0'} offer={["10%", "20%"]} img={foodpost} />
-          <CardComponent text={'باغ رستوران بهشت'} rate={'4.0'} offer={["10%", "20%"]} img={foodpost} />
-          <CardComponent text={'باغ رستوران بهشت'} rate={'4.0'} offer={["10%", "20%"]} img={foodpost} />
-          <CardComponent text={'باغ رستوران بهشت'} rate={'4.0'} offer={["10%", "20%"]} img={foodpost} />
+            <CardComponent
+              text={"باغ رستوران بهشت"}
+              rate={"4.0"}
+              offer={["10%", "20%"]}
+              img={foodpost}
+            />
+            <CardComponent
+              text={"باغ رستوران بهشت"}
+              rate={"4.0"}
+              offer={["10%", "20%"]}
+              img={foodpost}
+            />
+            <CardComponent
+              text={"باغ رستوران بهشت"}
+              rate={"4.0"}
+              offer={["10%", "20%"]}
+              img={foodpost}
+            />
+            <CardComponent
+              text={"باغ رستوران بهشت"}
+              rate={"4.0"}
+              offer={["10%", "20%"]}
+              img={foodpost}
+            />
           </div>
         </div>
         <div className="mt-10 mx-auto">
@@ -120,10 +228,30 @@ const Home: React.FC = () => {
             icon={<img className="w-7 h-7" src={teacher}></img>}
           />
           <div className="flex flex-row justify-between overflow-x-scroll">
-          <CardComponent text={'باغ رستوران بهشت'} rate={'4.0'} offer={["10%", "20%"]} img={foodpost} />
-          <CardComponent text={'باغ رستوران بهشت'} rate={'4.0'} offer={["10%", "20%"]} img={foodpost} />
-          <CardComponent text={'باغ رستوران بهشت'} rate={'4.0'} offer={["10%", "20%"]} img={foodpost} />
-          <CardComponent text={'باغ رستوران بهشت'} rate={'4.0'} offer={["10%", "20%"]} img={foodpost} />
+            <CardComponent
+              text={"باغ رستوران بهشت"}
+              rate={"4.0"}
+              offer={["10%", "20%"]}
+              img={foodpost}
+            />
+            <CardComponent
+              text={"باغ رستوران بهشت"}
+              rate={"4.0"}
+              offer={["10%", "20%"]}
+              img={foodpost}
+            />
+            <CardComponent
+              text={"باغ رستوران بهشت"}
+              rate={"4.0"}
+              offer={["10%", "20%"]}
+              img={foodpost}
+            />
+            <CardComponent
+              text={"باغ رستوران بهشت"}
+              rate={"4.0"}
+              offer={["10%", "20%"]}
+              img={foodpost}
+            />
           </div>
         </div>
         <div className="mt-10 mx-auto">
@@ -133,41 +261,65 @@ const Home: React.FC = () => {
             icon={<img className="w-7 h-7" src={teacher}></img>}
           />
           <div className="flex flex-row justify-between overflow-x-scroll">
-          <CardComponent text={'باغ رستوران بهشت'} rate={'4.0'} offer={["10%", "20%"]} img={foodpost} />
-          <CardComponent text={'باغ رستوران بهشت'} rate={'4.0'} offer={["10%", "20%"]} img={foodpost} />
-          <CardComponent text={'باغ رستوران بهشت'} rate={'4.0'} offer={["10%", "20%"]} img={foodpost} />
-          <CardComponent text={'باغ رستوران بهشت'} rate={'4.0'} offer={["10%", "20%"]} img={foodpost} />
+            <CardComponent
+              text={"باغ رستوران بهشت"}
+              rate={"4.0"}
+              offer={["10%", "20%"]}
+              img={foodpost}
+            />
+            <CardComponent
+              text={"باغ رستوران بهشت"}
+              rate={"4.0"}
+              offer={["10%", "20%"]}
+              img={foodpost}
+            />
+            <CardComponent
+              text={"باغ رستوران بهشت"}
+              rate={"4.0"}
+              offer={["10%", "20%"]}
+              img={foodpost}
+            />
+            <CardComponent
+              text={"باغ رستوران بهشت"}
+              rate={"4.0"}
+              offer={["10%", "20%"]}
+              img={foodpost}
+            />
           </div>
         </div>
-        </div>
-          {/* <div className="grid items-center justify-between grid-cols-4">
-            <CardComponent
-              text={"باغ رستوران بهشت"}
-              rate={"4.0"}
-              offer={["10%", "20%"]}
-              img={foodpost}
-            />
-            <CardComponent
-              text={"باغ رستوران بهشت"}
-              rate={"4.0"}
-              offer={["10%", "20%"]}
-              img={foodpost}
-            />
-            <CardComponent
-              text={"باغ رستوران بهشت"}
-              rate={"4.0"}
-              offer={["10%", "20%"]}
-              img={foodpost}
-            />
-            <CardComponent
-              text={"باغ رستوران بهشت"}
-              rate={"4.0"}
-              offer={["10%", "20%"]}
-              img={foodpost}
-            />
-          </div> */}
+      </div>
+      <div className="w-full bg-[#FFFFFF] mt-10 px-10 py-10">
+            <h1 className="text-lg font-semibold text-[#303030] p-5">
+            با اپلیکیشن آران آسایش، ساده‌تر از همیشه تخفیف بگیر!
+            </h1>
+            <p className="text-sm font-light text-[#717171] px-5 mb-3">
+            لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد.
+            </p>
+            <div className="flex flex-row justify-around items-end">
+              <img src={app_phone} className="w-4/12" alt="" />
+              <div className="my-10 flex flex-col">
+                      <Button
+                        className="flex bg-[#F5F5F5] text-gray-700 hover:!bg-transparent gap-1 items-center justify-center"
+                        placeholder={undefined}
+                      >
+                        <HiDownload className="w-6 h-6 text-[#7F38B7]" />
+                        <p className="block antialiased text-gray-700 text-lg font-semibold transition-colors bg-transparent">
+                          دانلود مستقیم
+                        </p>
+                      </Button>
+                      <Button
+                        className="flex mt-5 mt bg-[#F5F5F5] text-gray-700 hover:!bg-transparent gap-1 items-center justify-center"
+                        placeholder={undefined}
+                      >
+                        <img src={bazar} className="w-8 h-8" />
+                        <p className="block antialiased text-gray-700 text-lg font-semibold transition-colors bg-transparent">
+                          دانلود از کافه بازار
+                        </p>
+                      </Button>
+                    </div>
+            </div>
+      </div>
     </div>
-        
   );
 };
 export default Home;
--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -16,7 +16,10 @@ const CardComponent: React.FC<CardComponent> = ({ text, img, rate, offer }) => {
     >
       {/* <img className="-mr-[1.15rem] h-40 max-w-[16.2rem]" src={img} alt="" /> */}
       {/* <a href="#"> */}
-      <h5 className="text-lg mr-5 font-light tracking-tight text-[#303030] dark:text-white">
+      <div>
+        
+      </div>
+      <h5 className="text-lg font-light tracking-tight text-[#303030] dark:text-white">
         {text}
       </h5>
       {/* </a> */}
@@ -35,10 +38,10 @@ const CardComponent: React.FC<CardComponent> = ({ text, img, rate, offer }) => {
             <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
           </svg>
         </div>
-        <span className="mr-2 text-base font-extralight text-[#717171]">
+        <span className="mr-2 text-sm md:text-base font-extralight text-[#717171]">
           از{offer[0]} تا{" "}
-          <span className="text-base lg:text-xs font-extralight text-[#717171]">
-            <span className="text-[#7F38B7] lg:text-lg text-2xl font-semibold">
+          <span className="text-xs lg:text-base font-extralight text-[#717171]">
+            <span className="text-[#7F38B7] lg:text-lg text-xl font-semibold">
               {offer[1]}
             </span>{" "}
             تخفیف{" "}
